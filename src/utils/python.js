115e285e1c2a5f696b54e2efe592768d6a5f3ee7
Yashodhan Joshi <yjdoc2@gmail.com>
2020-10-05 19:40:30 +0530

    Change in Node for python for docker

--- src/utils/python.js
+++ src/utils/python.js
@@ -1,9 +1,10 @@
 // Global so can maintain state
 let counter = 0;
-const startPort = process.env.PYTHON_SERVER_PORTS_START;
-const numPorts = process.env.PYTHON_SERVER_NUMBER;
+const startContainer = process.env.PYTHON_SERVER_CONTAINER_START;
+const numContainer = process.env.PYTHON_SERVER_NUMBER;
 const rootPath = process.env.PYTHON_PORT_ROOT;
 const endpoint = process.env.PYTHON_SERVER_ENDPOINT;
+const retPort = process.env.PYTHON_PORT;
 /**
  * Returns url for next request to be made for code compiling
  * uses global counter variable
@@ -11,8 +12,8 @@ const endpoint = process.env.PYTHON_SERVER_ENDPOINT;
  * @return python server URL to be used for next code compile request
  */
 export const getPythonPath = () => {
-    let retPort = parseInt(startPort) + counter;
-    counter = (counter + 1) % numPorts;
+    let containerPath = parseInt(startContainer) + counter;
+    counter = (counter + 1) % numContainer;
 
-    return `${rootPath}:${retPort}/${endpoint}`;
+    return `${rootPath}${containerPath}:${retPort}/${endpoint}`;
 };
